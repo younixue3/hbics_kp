38a2448974cacb4b60476e1cba010f6957c94aac
syaifulrf <syaifulrf96@gmail.com>
2021-04-26 08:28:10 +0800

    a

--- resources/views/landing/juri.blade.php
+++ resources/views/landing/juri.blade.php
@@ -55,7 +55,7 @@
 @section('script')
 <script>
     $(".modal").on('hidden.bs.modal', function (e) {
-      $(".modal iframe").attr("src", $(".modal iframe").attr("src"));
-  });
+        $(".modal iframe").attr("src", $(".modal iframe").attr("src"));
+    });
 </script>
 @endsection
