a3ac50ad0d2968b718e38d3fec0057f773eccc85
younixue3 <bussiness.needs.rctka@gmail.com>
2021-11-10 16:01:26 +0800

    Fixing Database

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -44,16 +44,6 @@
                                     @enderror
                                 </div>
                             </div>
-                            {{-- <div class="form-group row">
-                                <div class="col-md-6 offset-md-4">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-                                        <label class="form-check-label" for="remember">
-                                            {{ __('Remember Me') }}
-                                        </label>
-                                    </div>
-                                </div>
-                            </div> --}}
                             <div class="form-group row">
                                 <div class="col-md-12 text-center">
                                     <br>
