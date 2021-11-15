8171de8404c57e717a6283342909eae2b6e70cf4
younixue3 <bussiness.needs.rctka@gmail.com>
2021-11-15 15:42:16 +0800

    Fixing Profile

--- app/Http/Controllers/ExpoController.php
+++ app/Http/Controllers/ExpoController.php
@@ -19,11 +19,13 @@ class ExpoController extends Controller
     {
         $this->middleware('auth');
     }
+
     public function profil()
     {
         $user = Auth::user();
         return view('expo.profil', compact('user'));
     }
+
     public function profilUpdate(Request $request)
     {
         //
@@ -31,42 +33,33 @@ class ExpoController extends Controller
         $validatedData = $request->validate([
             'name' => 'required',
             'email' => 'required',
-            ]);
+        ]);
         $user = User::findOrFail(Auth::user()->id);
-        $cekemail = User::where('email', $input['email'])->where('id', '!=' ,$user->id)->get();
-        if($cekemail->count() == 0)
-        {
-            if($input['password'] == '')
-            {
+        $cekemail = User::where('email', $input['email'])->where('id', '!=', $user->id)->get();
+        if ($cekemail->count() == 0) {
+            if ($input['password'] == '') {
                 $input['password'] = $user->password;
-            }
-            else
-            {
+            } else {
                 $input['password'] = Hash::make($input['password']);
             }
             $user->password = $input['password'];
             $user->email = $input['email'];
             $user->name = $input['name'];
             $user->save();
-            if($user)
-            {
+            if ($user) {
                 return redirect('profil')->with('success', 'Data berhasil diupdate di server');
-            }
-            else
-            {
+            } else {
                 return redirect('profil')->with('fail', 'Data gagal diupdate di server');
             }
-        }
-        else
-        {
+        } else {
             return redirect('profil')->with('fail', 'Data gagal diupdate di server, email telah terdaftar di akun lainnya');
         }
     }
+
     public function profilSimulasi()
     {
         $user = Auth::user();
-        if($user->role == 'admin' || $user->role == 'pengunjung')
-        {
+        if ($user->role == 'admin' || $user->role == 'pengunjung') {
             abort(404);
         }
         $karya = $user->karya;
@@ -75,53 +68,48 @@ class ExpoController extends Controller
         // exit;
         return view('expo.profil-simulasi', compact('user', 'event', 'karya'));
     }
+
     public function karyaUpdate(Request $request)
     {
         $input = $request->all();
         $user = Auth::user();
         $karya = Karya::where('user_id', $user->id)->latest()->first();
         // FOTO TIM
-        if($request->has('foto_tim'))
-        {
+        if ($request->has('foto_tim')) {
             $validatedData = $request->validate([
                 'foto_tim' => 'mimes:jpeg,bmp,png,jpg|max:2000',
             ]);
-            if($karya->foto_tim != '')
-            {
+            if ($karya->foto_tim != '') {
                 $this->deletefoto($karya->foto_tim);
             }
             $foto_tim = $input['foto_tim'];
-            $foto_timname = 'fotoTim-'.md5(\Carbon\Carbon::now().$foto_tim->getClientOriginalName()).'.'.$foto_tim->getClientOriginalExtension();
+            $foto_timname = 'fotoTim-' . md5(\Carbon\Carbon::now() . $foto_tim->getClientOriginalName()) . '.' . $foto_tim->getClientOriginalExtension();
             $foto_tim->move('uploads/karyas', $foto_timname);
             $input['foto_tim'] = $foto_timname;
         }
         // FOTO POSTER
-        if($request->has('foto_poster'))
-        {
+        if ($request->has('foto_poster')) {
             $validatedData = $request->validate([
                 'foto_poster' => 'mimes:jpeg,bmp,png,jpg|max:2000',
             ]);
-            if($karya->foto_poster != '')
-            {
+            if ($karya->foto_poster != '') {
                 $this->deletefoto($karya->foto_poster);
             }
             $foto_poster = $input['foto_poster'];
-            $foto_postername = 'fotoPoster-'.md5(\Carbon\Carbon::now().$foto_poster->getClientOriginalName()).'.'.$foto_poster->getClientOriginalExtension();
+            $foto_postername = 'fotoPoster-' . md5(\Carbon\Carbon::now() . $foto_poster->getClientOriginalName()) . '.' . $foto_poster->getClientOriginalExtension();
             $foto_poster->move('uploads/karyas', $foto_postername);
             $input['foto_poster'] = $foto_postername;
         }
         // PROPOSAL
-        if($request->has('proposal'))
-        {
+        if ($request->has('proposal')) {
             $validatedData = $request->validate([
                 'proposal' => 'mimes:docx,doc,pdf|max:2000',
             ]);
-            if($karya->proposal != '')
-            {
+            if ($karya->proposal != '') {
                 $this->deletefoto($karya->proposal);
             }
             $proposal = $input['proposal'];
-            $proposalname = 'proposal-'.md5(\Carbon\Carbon::now().$proposal->getClientOriginalName()).'.'.$proposal->getClientOriginalExtension();
+            $proposalname = 'proposal-' . md5(\Carbon\Carbon::now() . $proposal->getClientOriginalName()) . '.' . $proposal->getClientOriginalExtension();
             $proposal->move('uploads/karyas', $proposalname);
             $input['proposal'] = $proposalname;
         }
@@ -130,15 +118,13 @@ class ExpoController extends Controller
         // exit;
         $karya->update($input);
         $karya->save();
-        if($karya)
-        {
+        if ($karya) {
             return redirect('profil')->with('success', 'Data berhasil diupdate di server');
-        }
-        else
-        {
+        } else {
             return redirect('profil')->with('fail', 'Data gagal diupdate di server');
         }
     }
+
     public function karyaFoto(Request $request)
     {
         $input = $request->all();
@@ -148,54 +134,53 @@ class ExpoController extends Controller
         $user = Auth::user();
         $karya = Karya::where('user_id', $user->id)->latest()->first();
         $foto = $input['foto'];
-        $fotoname = 'foto-'.md5(\Carbon\Carbon::now().$foto->getClientOriginalName()).'.'.$foto->getClientOriginalExtension();
+        $fotoname = 'foto-' . md5(\Carbon\Carbon::now() . $foto->getClientOriginalName()) . '.' . $foto->getClientOriginalExtension();
         $foto->move('uploads/karyafotos', $fotoname);
         $input['foto'] = $fotoname;
         $input['karya_id'] = $karya->id;
         $data = KaryaFoto::create($input);
-        if($data)
-        {
+        if ($data) {
             return redirect('profil')->with('success', 'Foto berhasil diupload di server');
-        }
-        else
-        {
+        } else {
             return redirect('profil')->with('fail', 'Foto gagal diupload di server');
         }
     }
+
     public function karyaFotoDelete($id)
     {
         $data = KaryaFoto::findOrFail($id);
         $this->deletekaryafoto($data->foto);
         $data->delete();
-        if($data)
-        {
+        if ($data) {
             return redirect('profil')->with('success', 'Data berhasil diupdate di server');
-        }
-        else
-        {
+        } else {
             return redirect('profil')->with('fail', 'Data gagal diupdate di server');
         }
     }
-    public function deletefoto($fotoname){
+
+    public function deletefoto($fotoname)
+    {
         // path folder
         $path = 'uploads/karyas/';
         // delete gambar bisa jadikan if true or false misal false kasih konidisi etc
-        if(\File::delete($path.$fotoname)){
+        if (\File::delete($path . $fotoname)) {
             return true;
         }
     }
-    public function deletekaryafoto($fotoname){
+
+    public function deletekaryafoto($fotoname)
+    {
         // path folder
         $path = 'uploads/karyafotos/';
         // delete gambar bisa jadikan if true or false misal false kasih konidisi etc
-        if(\File::delete($path.$fotoname)){
+        if (\File::delete($path . $fotoname)) {
             return true;
         }
     }
+
     public function virtualexpo()
     {
-        if (Auth::user()->role == 'admin')
-        {
+        if (Auth::user()->role == 'admin') {
             $jenjang = 'smp';
             $kategori = 'kriya';
             switch ($jenjang) {
@@ -227,20 +212,16 @@ class ExpoController extends Controller
             $event = Event::where('status', 1)->latest()->first();
             $karyas = Karya::where('event_id', $event->id)->where('jenjang', $cjenjang)->where('kategori', $ckategori)->get();
             return view('expo.simulasi.expo-list', compact('event', 'karyas', 'jenjang', 'kategori', 'now'));
-        }
-        else
-        {
+        } else {
             abort(404);
         }
     }
+
     public function expoJenjangKategori($jenjang, $kategori)
     {
-        if (Auth::user()->role == 'admin')
-        {
-            if($jenjang == 'smp' || $jenjang == 'sma')
-            {
-                if($kategori == 'desain-grafis' || $kategori == 'aplikasi-dan-game' || $kategori == 'food-and-beverage' || $kategori == 'fashion' || $kategori == 'kriya')
-                {
+        if (Auth::user()->role == 'admin') {
+            if ($jenjang == 'smp' || $jenjang == 'sma') {
+                if ($kategori == 'desain-grafis' || $kategori == 'aplikasi-dan-game' || $kategori == 'food-and-beverage' || $kategori == 'fashion' || $kategori == 'kriya') {
                     switch ($jenjang) {
                         case 'smp':
                             $cjenjang = 'SMP/MTS';
@@ -270,30 +251,22 @@ class ExpoController extends Controller
                     $now = \Carbon\Carbon::now();
                     $karyas = Karya::where('event_id', $event->id)->where('jenjang', $cjenjang)->where('kategori', $ckategori)->get();
                     return view('expo.expo-list', compact('jenjang', 'kategori', 'karyas', 'now', 'event'));
-                }
-                else
-                {
+                } else {
                     abort(404);
                 }
-            }
-            else
-            {
+            } else {
                 abort(404);
             }
-        }
-        else
-        {
+        } else {
             abort(404);
         }
     }
+
     public function virtualexpoJenjangKategori($jenjang, $kategori)
     {
-        if (Auth::user()->role == 'admin')
-        {
-            if($jenjang == 'smp' || $jenjang == 'sma')
-            {
-                if($kategori == 'desain-grafis' || $kategori == 'aplikasi-dan-game' || $kategori == 'food-and-beverage' || $kategori == 'fashion' || $kategori == 'kriya')
-                {
+        if (Auth::user()->role == 'admin') {
+            if ($jenjang == 'smp' || $jenjang == 'sma') {
+                if ($kategori == 'desain-grafis' || $kategori == 'aplikasi-dan-game' || $kategori == 'food-and-beverage' || $kategori == 'fashion' || $kategori == 'kriya') {
                     switch ($jenjang) {
                         case 'smp':
                             $cjenjang = 'SMP/MTS';
@@ -323,26 +296,20 @@ class ExpoController extends Controller
                     $now = \Carbon\Carbon::now();
                     $karyas = Karya::where('event_id', $event->id)->where('jenjang', $cjenjang)->where('kategori', $ckategori)->get();
                     return view('expo.simulasi.expo-list', compact('jenjang', 'kategori', 'karyas', 'now', 'event'));
-                }
-                else
-                {
+                } else {
                     abort(404);
                 }
-            }
-            else
-            {
+            } else {
                 abort(404);
             }
-        }
-        else
-        {
+        } else {
             abort(404);
         }
     }
+
     public function expoDetailProduct($jenjang, $kategori, $id, $slug)
     {
-        if (Auth::user()->role == 'admin')
-            {
+        if (Auth::user()->role == 'admin') {
             $data = Karya::findOrFail($id);
             $karyas = Karya::where('jenjang', $data->jenjang)->where('kategori', $data->kategori)->where('id', '!=', $id)->get();
             switch ($data->jenjang) {
@@ -372,16 +339,14 @@ class ExpoController extends Controller
             }
             $statuslike = Komentar::where('user_id', Auth::user()->id)->where('karya_id', $id)->where('liked', 1)->latest()->first();
             return view('expo.expo-detail', compact('data', 'jenjang', 'kategori', 'karyas', 'statuslike'));
-        }
-        else
-        {
+        } else {
             abort(404);
         }
     }
+
     public function virtualexpoDetailProduct($jenjang, $kategori, $id, $slug)
     {
-        if (Auth::user()->role == 'admin')
-            {
+        if (Auth::user()->role == 'admin') {
             $data = Karya::findOrFail($id);
             $karyas = Karya::where('jenjang', $data->jenjang)->where('kategori', $data->kategori)->where('id', '!=', $id)->get();
             switch ($data->jenjang) {
@@ -411,9 +376,7 @@ class ExpoController extends Controller
             }
             $statuslike = Komentar::where('user_id', Auth::user()->id)->where('karya_id', $id)->where('liked', 1)->latest()->first();
             return view('expo.simulasi.expo-detail', compact('data', 'jenjang', 'kategori', 'karyas', 'statuslike'));
-        }
-        else
-        {
+        } else {
             abort(404);
         }
     }
