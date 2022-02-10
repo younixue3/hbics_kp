7795095e6e3ea4c4a53c8c58beaa77e99a847651
younixue3 <bussiness.needs.rctka@gmail.com>
2022-02-10 17:30:38 +0800

    403 forbidden

--- resources/views/admin/visitor/index.blade.php
+++ resources/views/admin/visitor/index.blade.php
@@ -256,8 +256,9 @@
                                                href="{{route('delete_visitor', $data->id)}}">Hapus</a>
                                             <a class="dropdown-item bg-warning text-white"
                                                href="{{route('show_visitor', $data->id)}}">Edit</a>
+
                                             <a class="dropdown-item bg-warning text-white"
-                                               href="{{ $data->karya != null ? asset('Upload/proposal/' . $data->karya->proposal) : '#'}}" target="{{ $data->karya != null ? '_blank' : ''}}">Lihat Proposal</a>
+                                               href="{{ $data->karya ? '#' : asset('Upload/proposal/' . $data->karya->proposal)}}" target="{{ $data->karya != null ? '_blank' : ''}}">Lihat Proposal</a>
                                             @if(Auth::User()->role == 'superadmin')
                                                 <a class="dropdown-item bg-primary text-white"
                                                    href="{{route('change_role_pengunjung', $data->id)}}">Jadikan
